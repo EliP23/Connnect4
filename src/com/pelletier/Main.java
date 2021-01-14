56296e433b359e18a2ba7234d295bcef4cfa8d2a
Eli Pelletier <elijahpaulpelletier@gmail.com>
2021-01-13 19:27:09 -0500

    Added horizontal win method

--- src/com/pelletier/Main.java
+++ src/com/pelletier/Main.java
@@ -9,7 +9,8 @@ public class Main {
         boolean win = false;
         int index;
         int turn=1;
-        while(win == false){
+        myBoard.printBoard();
+        while(!win){
             if(turn%2 != 0){
                 turn = 1;
             }else{
@@ -20,7 +21,7 @@ public class Main {
             myBoard.placePiece(turn, index);
             myBoard.printBoard();
             turn++;
-            //win = myBoard.checkWin();
+            win = myBoard.checkVerticalWin();
         }
     }
 }
--- src/com/pelletier/connectfour/Board.java
+++ src/com/pelletier/connectfour/Board.java
@@ -3,6 +3,8 @@ package com.pelletier.connectfour;
 public class Board {
     private int[][] array2d;
     private int height;
+    private boolean win;
+
     public Board (int length, int height){
         this.array2d = new int[height][length];
         this.height = height;
@@ -20,18 +22,38 @@ public class Board {
     public void placePiece(int team, int indexOfColumn){
         //this code needs to loop through and find whether or not the space beneath the current index is filled.
         //It will take a "team" to know which "color piece" to place, and the index of which column chosen
-        for(int i = 0; i <= height; i++){
-            if((array2d[i][indexOfColumn] != 0) || i == height){
+        for(int i = 1; i < height; i++){
+            if((array2d[i][indexOfColumn-1] != 0)){
                 if(team == 1){
-                    array2d[i-1][indexOfColumn]=1;
+                    array2d[i-1][indexOfColumn-1]=1;
                 }else{
-                    array2d[i-1][indexOfColumn]=8;
+                    array2d[i-1][indexOfColumn-1]=8;
+                }
+                break;
+            }else if(i+1 == height){
+                if(team == 1) {
+                    array2d[i][indexOfColumn-1]=1;
+                }else{
+                    array2d[i][indexOfColumn-1]=8;
                 }
             }
         }
     }
 
-    //public boolean checkWin(){return true;}
-
-
+    //Will go through each index like "placePiece" method.
+    //if selected piece and next 3 match, call win, else return false
+    public boolean checkHorizontalWin(){
+        for(int i = 0; i < array2d.length; i++) {
+            for (int j = 0; j <= height - 3; j++) {
+                if (array2d[i][j] != 0) {
+                    if ((array2d[i][j] == array2d[i][j + 1]) && (array2d[i][j + 1] == array2d[i][j + 2]) && (array2d[i][j + 2] == array2d[i][j + 3])) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
 }
+
+
